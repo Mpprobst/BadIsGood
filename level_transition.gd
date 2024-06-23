8a0d7d02bbbb70446f4cd8e24499272503ac7573
Tecunumon <stealth.32@hotmail.com>
2024-06-23 19:04:01 -0400

    Everything update

---
+++ level_transition.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+@onready var animation_player = $AnimationPlayer
+
+func fade_from_black():
+	animation_player.play("Fade_from_black")
+	await animation_player.animation_finished
+
+func fade_to_black():
+	animation_player.play("Fade_to_black")
+	await animation_player.animation_finished
