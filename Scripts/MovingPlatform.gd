68fa8057feb82f5a1a19d9b1a95085f88d374f8a
= <mpprobst@gmail.com>
2024-06-30 10:38:12 -0400

    Add 3 platforms

--- Scripts/MovingPlatform.gd
+++ Scripts/MovingPlatform.gd
@@ -1,32 +1,18 @@
 extends Node2D
 
-@export var speed = 1
-@export var length = 5
-@export var direction = Vector2.RIGHT
-
-var start_pos : Vector2
-var end_pos : Vector2
-var goal : Vector2
-var min_dist = 1	# how close to positions do we have to get
-
-var end = -1	#-1 means a start point, +1 means at end point
+@export var body : AnimatableBody2D
+@export var duration = 5
+@export var offset = Vector2(0, 300)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	start_pos = position
-	end_pos = start_pos + direction * length
-	goal = end_pos
+	#offset.y = -offset.y # its flipped for some reason
+	var tween = get_tree().create_tween().set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
+	tween.set_loops().set_parallel(false)
+	var start_pos = position
+	var end_pos = start_pos + offset
+	
+	tween.tween_property(body, "position", start_pos, duration / 2)
+	tween.tween_property(body, "position", end_pos, duration / 2)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	
-	var move = position.move_toward(goal, speed * delta)
-	if move.distance_to(goal) <= min_dist:
-		move = goal
-		end *= -1
-		if end == 1:
-			goal = end_pos
-		else:
-			goal = start_pos
-	
 
