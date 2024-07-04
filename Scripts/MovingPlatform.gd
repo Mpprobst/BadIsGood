3b059135c0ae22d33f0a459d0fd0d0647fae6f73
Michael Probst <mpprobst@gmail.com>
2024-07-04 09:57:02 -0400

    Add 8 test ice levels

--- Scripts/MovingPlatform.gd
+++ Scripts/MovingPlatform.gd
@@ -1,19 +1,20 @@
-extends Node2D
+class_name MovingPlatform
 
-@export var body : AnimatableBody2D
+extends AnimatableBody2D
+var body : AnimatableBody2D
 @export var duration = 5
 @export var offset = Vector2(0, 300)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#offset.y = -offset.y # its flipped for some reason
+	offset.y = -offset.y # its flipped for some reason
+	body = get_node(".")
 	var tween = get_tree().create_tween().set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
 	tween.set_loops().set_parallel(false)
-	var start_pos = global_position
+	var start_pos = position
 	var end_pos = start_pos + offset
-	
-	print("platform go to %s" % end_pos)
-	tween.tween_property(body, "global_position", end_pos, duration / 2.0)
-	tween.tween_property(body, "global_position", start_pos, duration / 2.0)
+
+	tween.tween_property(body, "position", end_pos, duration / 2.0)
+	tween.tween_property(body, "position", start_pos, duration / 2.0)
 
 
