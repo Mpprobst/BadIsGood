c3f70594d698efecc1123d303ebc28f5f78e2289
= <mpprobst@gmail.com>
2024-07-04 04:27:07 -0400

    some level tests

--- Scripts/MovingPlatform.gd
+++ Scripts/MovingPlatform.gd
@@ -9,10 +9,11 @@ func _ready():
 	#offset.y = -offset.y # its flipped for some reason
 	var tween = get_tree().create_tween().set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
 	tween.set_loops().set_parallel(false)
-	var start_pos = position
+	var start_pos = global_position
 	var end_pos = start_pos + offset
 	
-	tween.tween_property(body, "position", start_pos, duration / 2)
-	tween.tween_property(body, "position", end_pos, duration / 2)
+	print("platform go to %s" % end_pos)
+	tween.tween_property(body, "global_position", end_pos, duration / 2.0)
+	tween.tween_property(body, "global_position", start_pos, duration / 2.0)
 
 
